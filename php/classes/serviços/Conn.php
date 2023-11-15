16d72afdabce1b78e108a8de638ac99b0f6df085
enzomf <enzomfigueiredo11@gmail.com>
2023-11-15 20:33:25 -0300

    Update database connection credentials in Conn.php

--- php/classes/serviços/Conn.php
+++ php/classes/serviços/Conn.php
@@ -1,33 +1,42 @@
 <?php
-class Conn {
+class Conn
+{
     private static $instance = null;
-    private $host = "localhost";
-    private $user = "enzo";
-    private $password = "123456";
-    private $database = "php_aulainicial";
+    private $host = "db";
+    private $user = "p4";
+    private $password = "p4";
+    private $database = "p4";
     private $conn;
 
-    private function __construct() {
+    public function __construct()
+    {
         $this->conn = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        if (!$this->conn) {
+            throw new Exception("" . mysqli_connect_error());
+        }
+
         if (!$this->conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
     }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (self::$instance == null) {
             self::$instance = new Conn();
         }
         return self::$instance;
     }
 
-    public function getConnection() {
+    public function getConnection()
+    {
         return $this->conn;
     }
 
-    public function closeConnection() {
+    public function closeConnection()
+    {
         mysqli_close($this->conn);
     }
 }
- 
+
 ?>
