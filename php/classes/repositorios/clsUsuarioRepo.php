3b25010d807aa7998c34e089a5afa0bb51f77cb8
enzomf <enzomfigueiredo11@gmail.com>
2023-11-15 20:33:18 -0300

    Refactor UsuarioRepo class methods

--- php/classes/repositorios/clsUsuarioRepo.php
+++ php/classes/repositorios/clsUsuarioRepo.php
@@ -1,19 +1,23 @@
 <?php
 
-class UsuarioRepo {
+class UsuarioRepo
+{
     private $conexao;
 
-    public function __construct($conexao) {
+    public function __construct($conexao)
+    {
         $this->conexao = $conexao;
     }
 
-    public function listarTodos() {
+    public function listarTodos()
+    {
         $sql = "SELECT * FROM usuarios";
         $resultado = $this->conexao->query($sql);
         return $resultado->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function buscarPorId($id) {
+    public function buscarPorId($id)
+    {
         $sql = "SELECT * FROM usuarios WHERE id = :id";
         $stmt = $this->conexao->prepare($sql);
         $stmt->bindParam(':id', $id);
@@ -21,17 +25,31 @@ class UsuarioRepo {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function inserir($usuario) {
-        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
+    public function inserir(Usuario $usuario)
+    {
+
+        $nome = $usuario->getNome();
+        $email = $usuario->getEmail();
+        $senha = $usuario->getSenha();
+
+
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
         $stmt = $this->conexao->prepare($sql);
-        $stmt->bindParam(':nome', $usuario['nome']);
-        $stmt->bindParam(':email', $usuario['email']);
-        $stmt->bindParam(':senha', $usuario['senha']);
+        $stmt->bind_param('sss', $nome, $email, $senha);
+
+        if (!$stmt) {
+            die('Error preparing statement: ' . $this->conexao->error);
+        }
         $stmt->execute();
-        return $this->buscarPorId($this->conexao->lastInsertId());
+
+        if ($stmt->error) {
+            die('Error executing statement: ' . $stmt->error);
+        }
     }
 
-    public function atualizar($id, $usuario) {
+
+    public function atualizar($id, $usuario)
+    {
         $sql = "UPDATE usuarios SET nome = :nome, email = :email, senha = :senha WHERE id = :id";
         $stmt = $this->conexao->prepare($sql);
         $stmt->bindParam(':id', $id);
@@ -42,12 +60,25 @@ class UsuarioRepo {
         return $this->buscarPorId($id);
     }
 
-    public function deletar($id) {
+    public function deletar($id)
+    {
         $sql = "DELETE FROM usuarios WHERE id = :id";
         $stmt = $this->conexao->prepare($sql);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->rowCount() > 0;
+    }
+
+    public function buscarPorEmail($email)
+    {
+        $sql = 'SELECT nome, email, id FROM usuarios WHERE email=?';
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
     }
 }
 ?>
